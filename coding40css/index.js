e373f26d2492d4f208ad25f8bf1a6d59744a67a2
sumitbhskr <sumitbhaskar430@gmail.com>
2025-10-03 07:15:50 +0530

    Add coding40css

---
+++ coding40css/index.js
@@ -0,0 +1,42 @@
+// console.log("Hello I am conditional tutorial")
+
+let age = 0;
+// let grace =2;
+
+// age += grace
+// console.log(age)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+if (age!=18){
+    console.log("You can drive");
+}
+
+else if(age == 0){
+    console.log("Are you kidding");
+}
+
+else if(age ==1){
+    console.log("Are you again kidding");
+}
+else{
+    console.log("You cannot drive");
+}
+
+a = 6;
+b = 8;
+let c = a > b ? (a - b ) : (b - a);
+
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+} 
+else{
+     let c = a - b;
+    }
+     
+*/
