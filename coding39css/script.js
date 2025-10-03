6530c75199aedc4e94df53b1b709040782d5d61b
sumitbhskr <sumitbhaskar430@gmail.com>
2025-10-03 07:14:44 +0530

    Add coding39css

---
+++ coding39css/script.js
@@ -0,0 +1,52 @@
+console.log("Hey this is tutorial of js")
+
+// var a = 5;
+//  a = a + 1;
+// var b = 6;
+// var c = "Sumit";
+// var _a = "Shubham";
+// //var 55a = "Rohan"; // NOT Allowed
+
+// console.log(a + b + 8)
+// console.log(typeof a, typeof b, typeof c)
+ 
+// const a1 = 6;
+// a1 = a1 + 1; // NOT Allowed because a1 is Constant
+
+let a = 5;
+a = a + 1;
+let b = 6;
+let c = "Sumit";
+let _a = "Shubham";
+
+{   
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+
+
+
+let x = "Bhai";
+let y = 22;
+let z =3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    name: "Sumit",
+    "job code": 5666,
+    "is_handsome": true
+}
+    console.log(o);
+    o.salary ="50lakhs";
+    console.log(o);
+    o.salary = "60lakhs";
+    console.log(o);
+
