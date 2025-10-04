e23253b8e88a223732994a7a4aa934be4cfc7bdd
sumitbhskr <sumitbhaskar430@gmail.com>
2025-10-04 07:33:57 +0530

    Add coding45js

---
+++ coding45js/loops.js
@@ -0,0 +1,28 @@
+let a = [1, 93, 5, 6, 88]
+
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
+// }
+
+// a.forEach((value, indexedDB, arr)=>{
+//     console.log(value, indexedDB, arr) 
+// })
+
+// let obj ={
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+
+// for(const key in obj){
+//     if(Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key, element)
+// }
+// }
+
+for (const element of a) {
+    console.log(value)  
+}
