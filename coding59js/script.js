81b6c7a16f18f6064d0b33f8784b58732959779d
sumitbhskr <sumitbhaskar430@gmail.com>
2025-10-07 17:10:42 +0530

    Add coding59js

---
+++ coding59js/script.js
@@ -0,0 +1,37 @@
+async function sleep (){
+    return new Promise ((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve(45)
+        },1000);
+    }) 
+}
+
+function sum(a,b, c){
+    return a+b+c
+}
+( async function main(){ 
+    console.log(a1)  //declartion on the top
+//  let a = await sleep()      //iife imedetlly invoke function
+//  console.log(a);
+//  let b = await sleep()
+//  console.log(b);
+
+//    let [x, y, ...rest] = [1,5,7, 8, 9, 10]
+//    console.log(x, y, rest )
+
+   let obj = {
+    a: 1,
+    b: 2,
+    c: 3
+   }
+
+   let {a, b} = obj
+   console.log(a, b)
+
+   let arr = [1, 4, 6]
+   console.log(sum(arr[0], + arr[1], + arr[2]))
+   console.log(sum(...arr)) // open arr spreat opertor
+
+   var a1 = 6; //declartion on the top
+ //  let a1 = 6;
+})()
